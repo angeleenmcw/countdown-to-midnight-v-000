f64627c053ffe39ec5d84b7ab73b30e0f476df16
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-11 13:29:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,14 @@
 count = 10 
 
 def countdown(count)
-  until count > 0
+  while count > 0
   puts "#{number} SECOND(S)!"
   count -= 1
 end
 "HAPPY NEW YEAR!"
 end 
 
-def countdown_with_sleep
-  while 
+def countdown_with_sleep(count)
+  while count > 0 
+  puts "#{number} SECOND(S)!"
+  
